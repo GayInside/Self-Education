665372c5141ab601133e96ca19587d4bbbc1e234
Magn-Boy <andruha.vasilev.2004@mail.ru>
2023-11-13 18:34:47 +0300

    Created new EduPject

---
+++ ASP.NET-book/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using LanguageFeatures.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            Dictionary<string, Product> products = new Dictionary<string, Product>
+            {
+                { "Kayak", new Product {Name = "Kayak", Price = 275m} },
+                {"Lifejacket", new Product{Name = "Lifejacket", Price = 48.95m } }
+            };
+            return View("Index", products.Keys);
+        }
+    }
+}
---
+++ ASP.NET-book/LanguageFeatures/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+        public string Category { get; set; } = "Watersports";
+        public Product Related { get; set; }
+        public bool InStock { get; }
+
+        public Product(bool stock = true)
+        {
+            InStock = stock;
+        }
+        public static Product[] GetProducts()
+        {
+            Product kayak = new Product { Name = "kayak", Price = 275m, Category = "Water Craft" };
+
+            Product lifejacket = new Product(false) { Name = "Lifejacket", Price = 48.95m };
+
+            kayak.Related = lifejacket;
+            return new Product[] { kayak, lifejacket, null };
+        }
+    }
+}
